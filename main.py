3fc86b26b0da84dfeb827bda6111e519010d152e
Dana Bar Ilan <44768525+danab9@users.noreply.github.com>
2021-03-17 15:10:28 +0200

    added 'N' column

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ iter = file.pileup()
 #     #
 # print(positions_dict)
 
-df_pysam = pd.DataFrame(np.zeros(shape=(29903, 5)), columns=['C', 'A', 'G', 'T', 'isindel'], index=list(range(29903)))
+df_pysam = pd.DataFrame(np.zeros(shape=(29903, 6)), columns=['C', 'A', 'G', 'T', 'N', 'isindel'], index=list(range(29903)))
 
 MTpileup = file.pileup(stepper='nofilter')
 for position in MTpileup:
-    c = Counter({'C': 0, 'A': 0, 'G': 0, 'T': 0, 'isindel': 0})
+    c = Counter({'C': 0, 'A': 0, 'G': 0, 'T': 0, 'N': 0, 'isindel': 0})
     for pileupread in position.pileups:
         if not pileupread.is_del and not pileupread.is_refskip:
             c[pileupread.alignment.query_sequence[pileupread.query_position].upper()] += 1
