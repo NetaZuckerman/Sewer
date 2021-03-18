583a385b1bacd76557fb603577bf54f19c8078de
Dana Bar Ilan <44768525+danab9@users.noreply.github.com>
2021-03-18 11:39:08 +0200

    partial test 1

--- main.py
+++ main.py
@@ -4,32 +4,12 @@ import pandas as pd
 import numpy as np
 
 file = pysam.AlignmentFile('s6519_Ashdod_Jan1.mapped.sorted.bam', "rb")
+df_pysam = pd.DataFrame(np.zeros(shape=(29903, 5)), columns=['pos','C', 'A', 'G', 'T', 'isindel'], index=list(range(29903)))
+muttable = pd.read_csv("novelMutTable.csv")
 
-iter = file.pileup()
-# positions_dict = {i: dict() for i in range(0, 29903)}
-# mystring = ''
-# mylist = []
-# for pileupcol in file.pileup():
-#     mystring = ''
-#     for pileupread in pileupcol.pileups:
-#         if not pileupread.is_del and not pileupread.is_refskip:
-#             pos = pileupread.query_position
-#             # print(pos)
-#             if pileupread.alignment.query_sequence[pos] not in positions_dict[pos]:
-#                 positions_dict[pos][pileupread.alignment.query_sequence[pos]] = 0
-#             positions_dict[pos][pileupread.alignment.query_sequence[pos]] += 1
-#             # mystring = mystring + pileupread.alignment.query_sequence[pileupread.query_position]
-#
-#     # mystring = "".join(sorted(list(mystring)))
-#     # mylist.append((pileupcol.pos+1, mystring))
-#     #
-# print(positions_dict)
-
-df_pysam = pd.DataFrame(np.zeros(shape=(29903, 6)), columns=['C', 'A', 'G', 'T', 'N', 'isindel'], index=list(range(29903)))
-
-MTpileup = file.pileup(stepper='nofilter')
-for position in MTpileup:
-    c = Counter({'C': 0, 'A': 0, 'G': 0, 'T': 0, 'N': 0, 'isindel': 0})
+pileup_iter = file.pileup(stepper='nofilter')
+for position in pileup_iter:
+    c = Counter({'C': 0, 'A': 0, 'G': 0, 'T': 0, 'isindel': 0})
     for pileupread in position.pileups:
         if not pileupread.is_del and not pileupread.is_refskip:
             c[pileupread.alignment.query_sequence[pileupread.query_position].upper()] += 1
@@ -37,6 +17,15 @@ for position in MTpileup:
             c['isindel'] += 1
     df_pysam.loc[position.reference_pos+1] = pd.Series(c)
 
-file.close
-print(df_pysam)
-df_pysam.to_csv("pileup_N.csv")
+df_pysam.to_csv("pileup.csv")  # temporary file
+
+# create lineages list
+uniq_lineages = set()
+for lin in muttable.lineage:
+    for x in lin.split(','):
+        uniq_lineages.add(x.strip())
+
+# create dictionary: {lineage: muttable} (mutations table df for each lineage)
+muttable_by_lineage = {x: muttable[muttable.lineage.str.contains(x)] for x in uniq_lineages}
+
+# now theres a pileup dataframe. group by position, calculate sum
