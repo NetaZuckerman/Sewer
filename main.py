3c6e187dbdfbfeccd31f750b952eb7bf40fd9ca4
Dana Bar Ilan <44768525+danab9@users.noreply.github.com>
2021-03-18 11:47:40 +0200

    changed file name to compare

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 file = pysam.AlignmentFile('s6519_Ashdod_Jan1.mapped.sorted.bam', "rb")
-df_pysam = pd.DataFrame(np.zeros(shape=(29903, 6)), columns=['pos','C', 'A', 'G', 'T', 'isindel'], index=list(range(29903)))
+df_pysam = pd.DataFrame(np.zeros(shape=(29903, 5)), columns=['C', 'A', 'G', 'T', 'isindel'], index=list(range(29903)))
 muttable = pd.read_csv("novelMutTable.csv")
 
 pileup_iter = file.pileup(stepper='nofilter')
@@ -15,9 +15,9 @@ for position in pileup_iter:
             c[pileupread.alignment.query_sequence[pileupread.query_position].upper()] += 1
         if pileupread.indel:
             c['isindel'] += 1
-    df_pysam.loc[position.reference_pos+1] = pd.Series([position] + c)
+    df_pysam.loc[position.reference_pos+1] = pd.Series(c)
 
-df_pysam.to_csv("pileup.csv")  # temporary file
+df_pysam.to_csv("pileup.csv", index=False)  # temporary file
 
 # create lineages list
 uniq_lineages = set()
