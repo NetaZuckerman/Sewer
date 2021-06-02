a37b02f8a51e5e31c42f59db722ba3d9c8d81423
netanelxa <58979197+netanelxa@users.noreply.github.com>
2021-06-02 10:25:04 +0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -277,6 +277,7 @@ if __name__ == '__main__':
         print(final_df)
         print(final_df['Mutation'])
         print(final_df['Position'])
+        final_df.to_csv('yourfileout.csv', encoding='utf-8')
 
         final_df[file_name] = final_df.apply(lambda row: frequency(row['Mutation'], row['Position'] - 1, pileup_table, min_depth),
                                              axis=1)
