ded776a297258cb6664b1bf3766f4dd7d2c59482
Dana Bar Ilan <44768525+danab9@users.noreply.github.com>
2021-08-16 14:10:30 +0300

    added environment

--- mergeTwoTables.py
+++ mergeTwoTables.py
@@ -1,14 +1,16 @@
 import pandas as pd
 from sys import argv
 """
-merge compressed and not compressed tables.
+merge monitored mutations output tables.
 get two tables as input and create a new one called mergedTable.csv
 """
 
-table1 = pd.read_csv(argv[1])
-table2 = pd.read_csv(argv[2]).set_index(["Position", "Reference", "Mutation", "protein", "variant", "Mutation type",
+table1 = pd.read_csv('all.csv')
+table2 = pd.read_csv('ngs107.csv').set_index(["Position", "Reference", "Mutation", "protein", "variant", "Mutation type",
                                          "annotation", "varname", "lineage"])
 
-merged_table = table1.join(table2, how='outer', on=["Position", "Reference", "Mutation", "protein", "variant",
-                                                    "Mutation type", "annotation", "varname", "lineage"])
+merged_table = table1.join(table2, how='outer',
+                           on=["Position", "Reference", "Mutation", "protein", "variant", "Mutation type",
+                               "annotation", "varname", "lineage"]).fillna('X')
+
 merged_table.to_csv('mergedTable.csv')
